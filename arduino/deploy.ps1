99dd6105c8714248c408229e3c75b30ee70a2edb
ShiWenber <1210169842@qq.com>
2022-12-13 00:06:54 +0800

    docs(./deploy.ps1): 更新部署脚本

--- arduino/deploy.ps1
+++ arduino/deploy.ps1
@@ -1,7 +1,6 @@
 # jar包变化
 ssh -i C:\Users\shiwenbo\.ssh\ubuntu20_server_ali.pem root@boer.ink " cd /root/arduino;rm *.jar"
 scp -i C:\Users\shiwenbo\.ssh\ubuntu20_server_ali.pem .\target\*.jar root@boer.ink:/root/arduino
-ssh -i C:\Users\shiwenbo\.ssh\ubuntu20_server_ali.pem root@boer.ink " cd /root/arduino; docker compose build"
 
 # 重启镜像
-ssh -i C:\Users\shiwenbo\.ssh\ubuntu20_server_ali.pem root@boer.ink " cd /root/arduino;docker compose down; docker compose up -d"
+ssh -i C:\Users\shiwenbo\.ssh\ubuntu20_server_ali.pem root@boer.ink " cd /root/arduino;docker compose down;docker compose build; docker compose up -d"
