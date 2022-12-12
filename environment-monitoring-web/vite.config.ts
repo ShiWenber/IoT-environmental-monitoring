989525a9cc0210608bcdd76d38f23aa49360fd79
ShiWenber <1210169842@qq.com>
2022-12-13 00:11:52 +0800

    feat(./arduino/pom.xml): 调整前端到vue框架

---
+++ environment-monitoring-web/vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // 跨域设置
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-api-02.newbee.ltd/manage-api/v1', // 凡是遇到 /api 路径的请求，都映射到 target 属性
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
+      }
+    }
+  }
+})
