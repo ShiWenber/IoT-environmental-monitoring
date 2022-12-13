91b54c1798cbd0ee0e6b9497b465b43838b8e8cb
ShiWenber <1210169842@qq.com>
2022-12-13 14:49:50 +0800

    docs(.gitignore): 前端更新，完成了分页和登陆（明文）

--- environment-monitoring-web/config/index.ts
+++ environment-monitoring-web/config/index.ts
@@ -4,9 +4,9 @@ export default {
     baseUrl: '/api' // 开发代理地址
   },
   beta: {
-    baseUrl: '//backend-api-02.newbee.ltd/manage-api/v1' // 测试接口域名
+    baseUrl: '//' // 测试接口域名
   },
   release: {
-    baseUrl: '//backend-api-02.newbee.ltd/manage-api/v1' // 正式接口域名
+    baseUrl: '//' // 正式接口域名
   }
 }
